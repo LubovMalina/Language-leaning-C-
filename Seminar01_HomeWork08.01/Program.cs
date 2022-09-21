06ee67ff2f2ddb3cf863045d9e4a547274e4ef54
LubovMalina <andreeva.lubov27@gmail.com>
2022-09-21 18:42:23 +0300

    НЕ ЗАПУСКАЕТСЯ!!!!! Вывод четных чисел из диапозона от 1 до N

---
+++ Seminar01_HomeWork08.01/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int step = 2;
+
+while (num>=step)
+{
+    if (step%2==0)
+    {
+        Console.Write(step);
+        step +=1;
+    }
+} 
