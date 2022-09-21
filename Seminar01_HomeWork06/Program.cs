90f03dd0379d6ab8db2722613b84e2230edd2328
LubovMalina <andreeva.lubov27@gmail.com>
2022-09-21 17:47:03 +0300

    Определение четного и нечетного числа

---
+++ Seminar01_HomeWork06/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.Clear();
+
+Console.WriteLine("Введите число: ");
+double num = Convert.ToDouble(Console.ReadLine());
+
+if (num % 2 == 0)
+{
+    Console.WriteLine("Да, число четное");
+}
+else
+{
+    Console.WriteLine("Нет, число нечетное");
+}
