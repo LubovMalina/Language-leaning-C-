8a85facc108b3eb4dddb6c79971ccc1e7fc64fe9
LubovMalina <andreeva.lubov27@gmail.com>
2022-09-21 12:19:43 +0300

    Вывод последней цифры введенного числа

---
+++ Seminar01_4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+
+
+Console.WriteLine("Последняя цифра введенного числа " + number % 10);
+
+
