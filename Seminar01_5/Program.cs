ac2be1d99ca276dd9eeeb2d3f770ca7c72582119
LubovMalina <andreeva.lubov27@gmail.com>
2022-09-21 17:02:15 +0300

    Выведение чисел от -N до N

---
+++ Seminar01_5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите число: ");
+double number = Convert.ToDouble(Console.ReadLine());
+
+double number_neg = -(number);
+
+while (number_neg <= number)
+{
+   Console.WriteLine(number_neg);
+   number_neg++; 
+}
