5bd0c61ac36afca7f4d240740b8051c4b6dd8f6c
LubovMalina <andreeva.lubov27@gmail.com>
2022-09-21 20:39:45 +0300

    Сложение целых и деление вещественных чисел

---
+++ Examples003/Program.cs
@@ -0,0 +1,13 @@
+﻿//// Сложение двух чисел 
+
+int numberA = 31;
+int numberB = 5;
+int rezult = numberA + numberB;
+Console.WriteLine(rezult);
+
+
+////// 
+double numberC = 31;
+double numberK = 5;
+double r = (numberC / numberK);
+Console.WriteLine(r);
