a4e747d11d1f5ea8626836274d6a45dd6bbbe921
LubovMalina <andreeva.lubov27@gmail.com>
2022-09-21 20:46:25 +0300

    использования гениратора рандомных чисел

---
+++ Examples004/Program.cs
@@ -0,0 +1,9 @@
+﻿int number1 = new Random().Next(1,10);  // 0 1 2 3 ....9
+Console.WriteLine(number1);
+
+int number2 = new Random().Next(1,10);
+Console.WriteLine(number2);
+
+int rezult = number1 + number2;
+
+Console.WriteLine(rezult);
