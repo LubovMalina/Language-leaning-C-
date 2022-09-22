0df46a1e27535858d8b6118f96f114b60c13c150
LubovMalina <andreeva.lubov27@gmail.com>
2022-09-22 21:43:14 +0300

    нахождение длины отрезка

---
+++ Seminar03_03_2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите значение для точки А по оси х: ");
+int xa = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите значение для точки А по оси y: ");
+int ya = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите значение для точки B по оси х: ");
+int xb = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите значение для точки B по оси y: ");
+int yb = int.Parse(Console.ReadLine());
+
+double num = Math.Sqrt((Math.Pow((xb-xa),2))+(Math.Pow((yb-ya),2)));
+// d = √(xb - xa)2 + (yb - ya)2 =
+
+Console.Write("Расстояние между точками = ", num);
+Console.Write(num);
