f9e2773803c492826417ec1927c7bb24d40be844
LubovMalina <andreeva.lubov27@gmail.com>
2022-09-22 22:18:14 +0300

    возведение в степень числа от 1 до N

---
+++ Seminar03_04/Program.cs
@@ -0,0 +1,23 @@
+﻿// Console.Clear();
+
+Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int step = 1;
+
+if (num<1)
+{
+    Console.WriteLine("Некорректный ввод");
+}
+
+else
+{
+    while (num>=step)
+    {
+        Console.WriteLine(Math.Pow(step,2));
+        step++;
+    }
+}
+
+
